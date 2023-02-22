8c0a252a685208d1f7a1cddf24add41ef6b8bde0
i-webtest <stasbass80@gmail.com>
2023-02-22 11:45:46 +0300

    Created project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Catalog } from './components/Catalog/Catalog';
+import { Header } from './components/Header/Header';
+import { Navigation } from './components/Navigation/Navigation';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Navigation />
+        <Catalog />
+      </main>
+      <footer></footer>
+    </>
+  );
+};
